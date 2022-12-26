4c8bf03181464fb9070bbe48c901dd3d07bdb967
RosaMoran <rosa.moran.delgado@gmail.com>
2022-12-26 18:38:06 +0100

    adding new project into data and deployin in another server

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,8 @@ export { default as AVTR1 } from './portfolio1.png';
 export { default as AVTR2 } from './portfolio2.png';
 export { default as AVTR3 } from './portfolio3.png';
 export { default as AVTR4 } from './portfolio4.png';
+export { default as AVTR5 } from './portfolio5.png';
+
+
 export { default as Alina } from './Alina.jpg';
 export { default as Tom } from './Tom.jpg'; 
--- src/data/dataProject.js
+++ src/data/dataProject.js
@@ -1,33 +1,42 @@
-import {AVTR1, AVTR2, AVTR3, AVTR4 } from '../assets/index';
+import {AVTR1, AVTR2, AVTR3, AVTR4, AVTR5 } from '../assets/index';
 
 const data= [
     {
         id: 1,
-        image: AVTR1,
-        title: 'HYF-Connect',
-        github: 'https://github.com/HYF-Connect/hyf-connect',
-        demo: 'https://hyf-connect.herokuapp.com/'
+        image: AVTR5,
+        title: 'ChatGPT AI Application',
+        github: 'https://github.com/RosaMoran/codeAI',
+        demo: 'https://code-ai-amber.vercel.app/' 
+        
     },
     {
         id: 2,        
         image: AVTR2,
         title: 'Modern Web3',
         github: 'https://github.com/RosaMoran/modern-x',
-        demo: 'https://modern-x.herokuapp.com/#wgpt3'
+        demo: 'https://modern-x.vercel.app/'
     },
     {
         id: 3,
         image: AVTR3,
         title: 'Card Shop',
-        github: 'https://github.com/RosaMoran/shop-project',
-        demo: 'https://shop-projecttt.herokuapp.com/'
+        github: 'https://github.com/RosaMoran/shop-projectt',
+        demo: 'https://shop-projectt.vercel.app/'
     },
     {
         id: 4,
         image: AVTR4,
         title: 'Digital Clock',
         github: 'https://github.com/RosaMoran/Clock',
-        demo: 'https://rosaclock.herokuapp.com/'      
-    }]
+        demo: 'https://clock-rosamoran.vercel.app/'      
+    },
+    {
+        id: 5,
+        image: AVTR1,
+        title: 'HYF-Connect',
+        github: 'https://github.com/HYF-Connect/hyf-connect',
+        demo: 'https://hyf-connect.herokuapp.com/'
+    }
+]
 
     export default data;
